1f0dd24a21b9ee301d787114620270c7cee3868c
rodolfoIL <roizlam@gmail.com>
2024-10-18 02:32:59 +0200

    Add files via upload

---
+++ efecto-raton.js
@@ -0,0 +1,3 @@
+<!--Hide JavaScript from Java-Impaired Browsers //-->
+function NavRollOver(oTd){if(!oTd.contains(event.fromElement)){oTd.bgColor="#000000";}}
+function NavRollOut(oTd){if(!oTd.contains(event.toElement)){oTd.bgColor="transparent";}}
